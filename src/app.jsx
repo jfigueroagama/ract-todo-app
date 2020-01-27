1b6c1c5a157f758d81baec3dc818a43eb82a44bb
Josué Gama <josue.figueroa@magmalabs.io>
2020-01-27 13:29:21 -0600

    Initial implementation

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,18 @@
+class Todo extends React.Component {
+  render() {
+
+    let checked = (this.props.done == "true");
+
+    return <div className="todo">
+            <span>
+              <input type="checkbox" checked={checked}/>
+              <input type="text" value={this.props.text}/>
+            </span>
+          </div>;
+  }
+}
+
 ReactDOM.render(
-  <h1>Hello, world</h1>,
+  <Todo text="Test 1" done = "true" />,      //This is a constructor to create an object of the Todo class
   document.getElementById('root')
 );
